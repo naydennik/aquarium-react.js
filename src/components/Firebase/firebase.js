881cd09efd95076aa8bbae397e216fd0e68678a5
naydennik <naydennikolov@yahoo.com>
2020-09-03 22:13:09 +0300

    Implemented Firebase and created signup functionality

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,38 @@
+import app from "firebase/app";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyC0xwPps7b-hjP76xrovtkV8XtcdZGm8vk",
+  authDomain: "aquarium-a4f96.firebaseapp.com",
+  databaseURL: "https://aquarium-a4f96.firebaseio.com",
+  projectId: "aquarium-a4f96",
+  storageBucket: "aquarium-a4f96.appspot.com",
+  messagingSenderId: "369881047370",
+  appId: "1:369881047370:web:e26a6380c2066a0aaa61b3",
+  measurementId: "G-8MG08BDZ1E",
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
