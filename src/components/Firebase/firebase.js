6134ee0896963c3c5a2d59c8e1b5629015a3b65c
naydennik <naydennikolov@yahoo.com>
2020-09-29 08:10:21 +0300

    Created .env file with Firebase credentials

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-  apiKey: "AIzaSyC0xwPps7b-hjP76xrovtkV8XtcdZGm8vk",
-  authDomain: "aquarium-a4f96.firebaseapp.com",
-  databaseURL: "https://aquarium-a4f96.firebaseio.com",
-  projectId: "aquarium-a4f96",
-  storageBucket: "aquarium-a4f96.appspot.com",
-  messagingSenderId: "369881047370",
-  appId: "1:369881047370:web:e26a6380c2066a0aaa61b3",
-  measurementId: "G-8MG08BDZ1E",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID,
 };
 
 class Firebase {
